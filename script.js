2f86d357bfbbc12ee98355d4edd15b9563e5529f
Thor Christensen <thorchristensen@Thors-MacBook-Pro.local>
2020-11-15 18:41:23 +0100

    v1.1

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-function myFunction() {
-    var x = document.getElementById("container");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-}
+    // const erklære en variabel med værdien burger, nav-links, nav-links li///
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+
+
+    burger.addEventListener('click', () => {
+        //Toggle Nav
+        nav.classList.toggle('nav-active');
+
+
+
+        //Animate Links (forEach går i gennem hvert link/Element)
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                //(hvis der allerede er en animation så fjernes den)
+                link.style.animation = ''
+            } else {
+                //(hvis der ikke er en animation, så tilføjes den
+                link.style.animation = `navLinkFade 0.5s ease forwards ${(index / 6)+.4}s`;
+            }
+        });
+        //Burger Animation (her fjernes eller påsættes toggle-klassen fra css)
+        burger.classList.toggle('toggle');
+
+
+
+    });
